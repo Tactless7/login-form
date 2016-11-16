110d5ccbfb1907df1acd61af22c27c5faa6cc8e7
Juliane Blier <blier.juliane@gmail.com>
2016-11-16 16:35:20 +0100

    Messages d'erreurs opérationnels si mauvais mdp ou username

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-var express = require ('express');
+var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(2605, function(){
 	console.log('I am watching port 2605');
 });
+
+var trueUsername = 'root' ;
+var truePassword = 'admin' ;
+
+app.post('/login', function(req, res){
+	var login = req.body ;
+	if(login.username === trueUsername && login.password === truePassword){
+		res.send('You\'re successfully logged !');
+	}
+	else {
+		res.send('Error, try again.');
+	}
+
+});
+
